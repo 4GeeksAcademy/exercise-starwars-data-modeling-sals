ade79129b8375ade31e6f1dec8141ac4099d140d
sebastianlopezs <135356217+sebastianlopezs@users.noreply.github.com>
2023-09-09 16:15:01 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -1,32 +1,65 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
+
     id = Column(Integer, primary_key=True)
+
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    username = Column(String(80), nullable=False)
+    email = Column(String(256), nullable=False, unique=True)
+
+    @classmethod
+    def accelerate(cls):
+        pass
+
+
+class Planets(Base):
+
+    __tablename__ = "planets"
+
+    planet_id = Column(Integer, primary_key=True)
+    name = Column(String(180), nullable=False)
+    atmosphere = Column(Boolean(500), nullable=True)
+
+
+
+class Characters(Base):
+
+    __tablename__ = 'characters'
+
+    planet_id = Column(Integer, primary_key=True)
+    full_name = Column(String(300), nullable=False)
+    hair_color= Column(String(30), nullable=False)
+    eye_color= Column(String(30), nullable=False)
+
+class Favorite(Base):
+    __tablename__="favorites"
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    user_id= Column (Integer, ForeignKey('user.id'), nullable=False)
+    user= relationship(User)
 
-## Draw from SQLAlchemy base
+    character_id= Column(Integer, ForeignKey('characters.id'), nullable=True)
+    character = relationship(Characters)
+
+    planet_id = Column (Integer, ForeignKey('planets.id'), nullable=True)
+    planet = relationship(Planets)
+
 render_er(Base, 'diagram.png')
+
+
+
+   
